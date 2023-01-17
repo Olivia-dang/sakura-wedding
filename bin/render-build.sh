8841c4c68ac4920899319bd77208f9daed88d37a
Olivia Dang <78009350+oliviadangpad@users.noreply.github.com>
2023-01-17 23:33:57 +1100

    try4

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 bundle install
 # bundle exec rails webpacker:install
+RAILS_ENV=test bundle exec rake assets:precompile
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
